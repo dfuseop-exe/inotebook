3599b20401889818e83f99f9435f2208f40ff0ea
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-01 10:42:39 +0530

    connect to mongoose

---
+++ Backend/db.js
@@ -0,0 +1,11 @@
+const mongoose= require('mongoose')
+
+const mongooseURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+
+const connectToMongo = () => {
+    mongoose.connect(mongooseURI , ()=>{
+        console.log("Connect to mangoose Successfully")
+    })
+}
+
+module.exports = connectToMongo  ;

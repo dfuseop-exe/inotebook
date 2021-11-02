4e24e9dcd3c162a3babd0b9ed59e7d69bde5d29c
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-02 16:24:57 +0530

    Added Middleware

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/' , (req , res )=>{
-    res.json([]);
+    console.log(req.body)
+    res.send("SuccessFull")
 })
 
 

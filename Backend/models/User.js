76520d36c10fee2156113e3d6248d8e3b9f69cef
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-03 11:08:22 +0530

    Storing data in mongoDB

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,23 +1,24 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name : {
-        type : String ,
-        require : true ,
+    name:{
+        type: String,
+        required: true
     },
-    email : {
-        type : String ,
-        require : true ,
-        unique : true ,
+    email:{
+        type: String,
+        required: true,
+        unique: true
     },
-    password : {
-        type : String ,
-        require : true ,
+    password:{
+        type: String,
+        required: true
     },
-    date : {
-        type : Date ,
-        default : Date.now ,
-    }
-})
+    date:{
+        type: Date,
+        default: Date.now
+    },
+  });
 
-module.exports = mongoose.model('user', UserSchema)
+  module.exports = mongoose.model('user', UserSchema);

e73d84e3349dc140f0e26e596f5e16910145f04a
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-03 12:03:22 +0530

    Added Data validation using Express validator

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res)=>{ 
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
+router.post('/',[
+    body('email' , 'Enter valid Email').isEmail(),
+    body('name' , 'Name must be atleast 5 character long').isLength({ min: 5 }),
+    body('password' , 'Password must be atleast 8 character long').isLength({ min: 8 }),
+],(req, res)=>{ 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      }).then(user => res.json(user))
+      .catch(err=>{
+        res.json({error : "Please Enter unique value for email" , message : err.message})
+      })
 } )
 
 

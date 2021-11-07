a86c3c51c89ee4775b9c7a96c725c43a42117acc
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-07 12:27:43 +0530

    wrong  commit for checking error

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,9 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+var fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/notes");
+const { body, validationResult } = require("express-validator");
 
-router.get('/' , (req , res )=>{
-    res.json([]);
-})
+// ROUTE 1:Get all notes using: GET "/api/auth/fetchallnotes".login required
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+ try {
+    const notes = await Notes.find({ user: req.user.id });
+    res.json(notes);
+ } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+}
+});
 
+// ROUTE 2: Add a new Note using: POST "/api/auth/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
+        try {
 
-module.exports = router ;
+            const { title, description, tag } = req.body;
+
+            // If there are errors, return Bad request and the errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+
+            res.json(savedNote)
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    })
+
+module.exports = router;

41b4f2bca5c711d9df4646d3950e6d603c157f01
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-07 23:26:32 +0530

    Creating Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-
 import './App.css';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Inotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route> 
+        </Switch>
+      </Router>
+    </>
   );
 }
 

7ea096c26d256357e313f44cfe8de12ec510a4b4
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-11-09 16:02:48 +0530

    fetch notes functionality is added

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,22 +1,25 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
-import Noteitem from "./NoteItem";
-import AddNote from "./AddNote";
+import React, {useContext, useEffect} from 'react'
+import noteContext from "../context/notes/noteContext"
+import Noteitem from './NoteItem';
+import AddNote from './AddNote';
 
 const Notes = () => {
-  const context = useContext(noteContext);
-  const { notes} = context;
-  return (
-    <div className="container">
-      <AddNote />
-      <div className="row my-3">
-        <h2>You Notes</h2>
-        {notes.map((note) => {
-          return <Noteitem key={note._id} note={note} />;
-        })}
-      </div>
-    </div>
-  );
-};
+    const context = useContext(noteContext);
+    const {notes, getNotes} = context;
+    useEffect(() => {
+        getNotes()
+    }, [])
+    return (
+        <>
+        <AddNote/>
+        <div className="row my-3">
+            <h2>You Notes</h2> 
+            {notes.map((note)=>{
+                return <Noteitem key={note._id} note={note}/>  
+            })}
+            </div>
+            </>
+    )
+}
 
-export default Notes;
+export default Notes
